edf586e3ba5e2990eb360cc9ce82caf08e711389
stephmarky <smarki340@gmail.com>
2015-02-08 00:03:24 -0800

    Deleted Swiping Stuff

---
+++ Mate/Yar.js
@@ -0,0 +1,31 @@
+var currentFile = "yar.wav";
+        function playAudio() {
+            // Check for audio element support.
+            if (window.HTMLAudioElement) {
+                try {
+                    var oAudio = document.getElementById('myaudio');
+                    var btn = document.getElementById('play'); 
+                    var audioURL = document.getElementById('audiofile'); 
+
+                    //Skip loading if current file hasn't changed.
+                    if (audioURL.value !== currentFile) {
+                        oAudio.src = audioURL.value;
+                        currentFile = audioURL.value;                       
+                    }
+
+                    // Tests the paused attribute and set state. 
+                    if (oAudio.paused) {
+                        oAudio.play();
+                        btn.textContent = "Pause";
+                    }
+                    else {
+                        oAudio.pause();
+                        btn.textContent = "Play";
+                    }
+                }
+                catch (e) {
+                    // Fail silently but show in F12 developer tools console
+                     if(window.console && console.error("Error:" + e));
+                }
+            }
+        }
